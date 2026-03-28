f415dbe613bf2a3868cabbdb7e7e0977785859cb
hwdemtv <hwdemtv@qq.com>
2026-03-28 15:42:17 +0800

    chore: bump version to 0.1.6-beta

--- src/config/version.py
+++ src/config/version.py
@@ -1,5 +1,5 @@
 # ZenClean 全局版本信息
-__version__ = "0.1.5-beta"
+__version__ = "0.1.6-beta"
 __app_name__ = "ZenClean"
 __display_name__ = "禅清 (ZenClean)"
-__build_date__ = "2026-03-11"
+__build_date__ = "2026-03-28"
